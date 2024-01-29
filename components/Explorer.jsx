b5a26c6257b72081625fa69b9e782290a545e5b4
Abdullah Khan <abdkhan033@gmail.com>
2024-01-29 03:09:39 -0500

    Initial Commit

--- components/Explorer.jsx
+++ components/Explorer.jsx
@@ -26,6 +26,11 @@ const explorerItems = [
     icon: 'js_icon.svg',
   },
   {
+    name: 'articles.json',
+    path: '/articles',
+    icon: 'json_icon.svg',
+  },
+  {
     name: 'github.md',
     path: '/github',
     icon: 'markdown_icon.svg',
